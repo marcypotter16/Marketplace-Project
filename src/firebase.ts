d25ee5d31b6735badc5bca205ec50034b15f7d93
Marcello <106889248+marcypotter16@users.noreply.github.com>
2023-07-15 21:43:59 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,19 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/storage';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyBv3LVTlzNV4PsXlYbT4Eg8Y24o7n4lhvY',
-
-  authDomain: 'react-marketplace-b3171.firebaseapp.com',
-
-  projectId: 'react-marketplace-b3171',
-
-  storageBucket: 'react-marketplace-b3171.appspot.com',
-
-  messagingSenderId: '320185275256',
-
-  appId: '1:320185275256:web:39c6b5c8be3cab6fc32e8d',
-
-  measurementId: 'G-MENL2CHTCE',
+  // Your keys
 });
 
 export const db = firebase.firestore();
