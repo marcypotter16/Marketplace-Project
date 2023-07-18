cb2a058d4c312cdfd97579d55845cb7addced33d
Potter <marcypotter16@gmail.com>
2023-07-18 11:17:13 +0200

    yo

--- src/Utils/firebase.ts
+++ src/Utils/firebase.ts
@@ -4,19 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 initializeApp({
-  apiKey: 'AIzaSyBv3LVTlzNV4PsXlYbT4Eg8Y24o7n4lhvY',
-
-  authDomain: 'react-marketplace-b3171.firebaseapp.com',
-
-  projectId: 'react-marketplace-b3171',
-
-  storageBucket: 'react-marketplace-b3171.appspot.com',
-
-  messagingSenderId: '320185275256',
-
-  appId: '1:320185275256:web:39c6b5c8be3cab6fc32e8d',
-
-  measurementId: 'G-MENL2CHTCE',
+  // Your web app's Firebase configuration
 });
 
 export const db = getFirestore();
